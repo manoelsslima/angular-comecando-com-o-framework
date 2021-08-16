1dc837f5f14b6c774e4f62dcfb1b08ada523d690
Manoel de Souza Silva Lima <manoel.lima@tjac.jus.br>
2021-08-16 02:30:24 -0500

    Trabalhando com Service. Diretiva *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,13 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private service: TransferenciaService) {}
+
   title = 'bytebank';
 
-  transferencias: any[] = []; // precisa inicializar o array antes de fazer push
-
+  // recebe o event e propaga pro service
   transferir($event: any) {
-    console.log($event);
-    const transferencia = {...$event, data: new Date()}
-    this.transferencias.push(transferencia);
+    this.service.adicionar($event);
   }
 }
