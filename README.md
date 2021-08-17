24a1b07a442ba2bf42aa2b18144701e308a4c6de
Manoel de Souza Silva Lima <manoel.lima@tjac.jus.br>
2021-08-16 23:58:19 -0500

    Finalização do projeto em Angular

--- README.md
+++ README.md
@@ -26,3 +26,9 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
 # Angular: Começando com o framework
+
+## Subindo o servidor mock de json
+
+Entre no diretório onde está o arquivo db.json e execute:
+
+$ json-server --watch db.json
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -2,6 +2,7 @@ import { Transferencia } from './../models/transferencia.model';
 import { TransferenciaService } from './../services/transferencia.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -15,7 +16,7 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
-  constructor(private service: TransferenciaService){}
+  constructor(private service: TransferenciaService, private router: Router){}
 
   transferir() {
     console.log('Solicitada nova transferência');
@@ -26,6 +27,7 @@ export class NovaTransferenciaComponent {
       (resultado) => {
       console.log(resultado);
       this.limparCampos();
+      this.router.navigateByUrl('extrato'); // redireciona
     },
     (error) => console.error(error)
     );
